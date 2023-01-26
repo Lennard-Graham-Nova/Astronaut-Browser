3ce7bb98cf4cc13bfca51d0b74ced29d78da3e0c
lennardgraham-64 <lennardgraham@ustwo.com>
2023-01-26 03:24:55 +0000

    Initial Commit

---
+++ Astronaut Browser/Astronaut_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Astronaut_BrowserApp.swift
+//  Astronaut Browser
+//
+//  Created by Lennard Graham on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Astronaut_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
